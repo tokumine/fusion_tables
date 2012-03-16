c37bfb16e1c4d411a767a51185d2c0db2542c255
tomykaira <tomykaira@gmail.com>
2012-03-16 20:04:30 +0900

    whitespace

--- lib/fusion_tables/ext/fusion_tables.rb
+++ lib/fusion_tables/ext/fusion_tables.rb
@@ -14,89 +14,89 @@
 
 module GData
   module Client
-    class FusionTables < Base    
-                     
+    class FusionTables < Base
+
       # Helper method to run FT SQL and return FT data object
-      def execute(sql)        
+      def execute(sql)
         http_req = sql.upcase.match(/^(DESCRIBE|SHOW|SELECT)/) ? :sql_get : :sql_post
-        GData::Client::FusionTables::Data.parse(self.send(http_req, sql)).body                    
-      end                                                   
-                          
-      # Show a list of fusion tables
-      def show_tables 
-        data = self.execute "SHOW TABLES"
-        
-        data.inject([]) do |x, row|
-          x << GData::Client::FusionTables::Table.new(self, row)        
-          x
-        end  
+        GData::Client::FusionTables::Data.parse(self.send(http_req, sql)).body
       end
-            
+
+      # Show a list of fusion tables
+      def show_tables
+        data = self.execute "SHOW TABLES"
+
+        data.inject([]) do |x, row|
+          x << GData::Client::FusionTables::Table.new(self, row)
+          x
+        end
+      end
+
       # Create a new table. Return the corresponding table
-      # 
+      #
       # Columns specified as [{:name => 'my_col_name', :type => 'my_type'}]
       #
       # Type must be one of:
-      # 
-      # * number 
-      # * string 
+      #
+      # * number
+      # * string
       # * location
       # * datetime
       #
       def create_table(table_name, columns)
-        
+
         # Sanity check name
         table_name = table_name.strip.gsub(/ /,'_')
-        
+
         # ensure all column types are valid
         columns.each do |col|
           if !DATATYPES.include? col[:type].downcase
             raise ArgumentError, "Ensure input types are: 'number', 'string', 'location' or 'datetime'"
-          end  
+          end
         end
-        
+
         # generate sql
         fields = columns.map{ |col| "'#{col[:name]}': #{col[:type].upcase}" }.join(", ")
         sql = "CREATE TABLE #{table_name} (#{fields})"
-        
-        # create table        
+
+        # create table
         resp = self.sql_post(sql)
         raise "unknown column type" if resp.body == "Unknown column type."
-        
-        # construct table object and return        
+
+        # construct table object and return
         table_id = resp.body.split("\n")[1].chomp.to_i
         table = GData::Client::FusionTables::Table.new(self, :table_id => table_id, :name => table_name)
         table.get_headers
         table
       end
-      
+
       # Drops Fusion Tables
       #
       # options can be:
       #
-      # * an integer for single drop 
+      # * an integer for single drop
       # * array of integers for multi drop
-      # * a regex against table_name for flexible multi_drop 
+      # * a regex against table_name for flexible multi_drop
       #
       def drop(options)
         # collect ids
         ids = []
         ids << options  if options.class == Integer || options.class == String || Fixnum
         ids =  options  if options.class == Array
-        
+
         if options.class == Regexp
           tables = show_tables
           ids = tables.map { |table| table.id if options =~ table.name }.compact
-        end       
-        
-        # drop tables  
-        delete_count = 0
-        ids.each do |id|  
-          resp = self.sql_post("DROP TABLE #{id}") 
-          delete_count += 1 if resp.body.strip.downcase == 'ok'          
         end
-        delete_count      
-      end        
+
+        # drop tables
+        delete_count = 0
+        ids.each do |id|
+          resp = self.sql_post("DROP TABLE #{id}")
+          delete_count += 1 if resp.body.strip.downcase == 'ok'
+        end
+        delete_count
+      end
     end
   end
 end
--- test/test_ext.rb
+++ test/test_ext.rb
@@ -1,60 +1,60 @@
 require 'helper'
 
 class TestExt < Test::Unit::TestCase
- 
+
   context "The Fusion Tables helper functions" do
-    setup do 
+    setup do
       init_config
-      @ft = GData::Client::FusionTables.new      
+      @ft = GData::Client::FusionTables.new
       @ft.clientlogin(username, password)
     end
-  
-    
+
+
     should "raise ArgumentError if supply unknown types to it" do
       assert_raise ArgumentError do
         @ft.create_table "test table", [{:name => "test_col", :type => "billys birthday" }]
       end
-    end  
-  
+    end
+
     should "let you create a table if you get everything right" do
       table = @ft.create_table "test_table", [{:name => "test_col", :type => "string" }]
       assert_equal GData::Client::FusionTables::Table, table.class
       @ft.drop(table.id)
     end
-    
+
     should "correct your table name to a certain degree on create" do
       table = @ft.create_table "test table", [{:name => "test col", :type => "string" }]
       assert_equal "test_table", table.name
       @ft.drop(table.id)
     end
-    
+
     should "return you a list of your fusion tables" do
       resp = @ft.show_tables
       assert_equal GData::Client::FusionTables::Table, resp.first.class if resp.first
     end
-    
+
     should "be possible to delete a table with an id" do
       table = @ft.create_table "test_table", [{:name => "test col", :type => "string" }]
       assert_equal 1, @ft.drop(table.id)
     end
-    
+
     should "be possible to delete tables with an array of ids" do
       table1 = @ft.create_table "test_table", [{:name => "test col", :type => "string" }]
       table2 = @ft.create_table "test_table", [{:name => "test col", :type => "string" }]
       assert_equal 2, @ft.drop([table1.id, table2.id])
     end
-     
+
     should "be possible to delete multiple tables with a regex" do
       table1 = @ft.create_table "test_table", [{:name => "test col", :type => "string" }]
       table2 = @ft.create_table "test_table", [{:name => "test col", :type => "string" }]
       assert_equal 2, @ft.drop(/^test_/)
     end
-       
+
     should "return zero if passed a silly id" do
       assert_raise GData::Client::BadRequestError do
         assert_equal 0, @ft.drop(235243875629384756)
       end
-    end    
+    end
   end
 
 end
